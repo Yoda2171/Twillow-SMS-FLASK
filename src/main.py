299aacb991f80da96cdc5846f76d904d4eb8564a
Yoda2171 <popotaya7@gmail.com>
2021-02-09 23:11:37 -0300

    Lista de espera Bonito

--- src/main.py
+++ src/main.py
@@ -2,23 +2,24 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+from twilio.rest import Client
 from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
+
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
+
+from models import Queue
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db)
-db.init_app(app)
 CORS(app)
-setup_admin(app)
+
+
+_queue = Queue()
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -30,14 +31,43 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/all', methods=['GET'])
+def todos():
+    
+    usuario = _queue.get_queue()
+    return jsonify(usuario),200 
+   
+    
+@app.route('/next', methods=['DELETE'])
+def siguente():
+   
+    usuario = _queue.dequeue()
+    return jsonify({"success": "Deleted Complete"}),200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/new', methods=['POST'])
+def creador():
+    
+
+    name = request.json.get("name")
+    phone = request.json.get("phone")
+
+    item = {
+        "name" : name,
+        "phone" : phone
+    }   
+
+    if not name :
+        return({"msg" : "coloca un nombre"})
+    if not phone :
+        return({"msg" : "coloca un phone"})
+
+    _queue.enqueue(item)
+
+    return({"msg" : "send complete"})
+
+    
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,51 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+from twilio.rest import Client
 
-db = SQLAlchemy()
+class Queue:
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    def __init__(self):
+        self.account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        self.auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        self.client = Client(self.account_sid, self.auth_token)
+        self._queue = []
+        self._mode = 'FIFO'
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def enqueue(self, item):
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        if self.size() == 0:
+            message = self.client.messages.create (
+                body = str(item["name"]) + ", es su turno",
+                from_='+12055578744',
+                to='+56 9 6632 7166',
+            )          
+        
+        else:
+            message = self.client.messages.create (
+                body='Bienvenidos, ' +  str(item["name"]) + " tu tienes " + str(self.size()) + " personas por delante de usted, espere su turno",
+                from_='+12055578744',
+                to='+56 9 6632 7166',
+            )
+
+        self._queue.append(item)
+
+        
+        
+    
+    def dequeue(self):
+        
+        item = self._queue.pop(0)
+
+        message = self.client.messages.create(
+            body= item["name"] + " Muchas Gracias!!, que tengan un buen dia",
+            from_ = "+12055578744",
+            to = '+56 9 6632 7166',            
+            )
+
+
+    def get_queue(self):
+
+        return self._queue
+
+
+    def size(self):
+        return len(self._queue)
